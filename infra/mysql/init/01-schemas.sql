5db3ef268a11eb87aae4e07430b1326923b27a40
woo jin <dnwls16071@naver.com>
2026-09-23 01:28:46 +0900

    build: Gradle 멀티모듈 골격과 로컬 인프라 구성

---
+++ infra/mysql/init/01-schemas.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS `order`   CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `product` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `point`   CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `payment` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE USER IF NOT EXISTS 'order_user'@'%'   IDENTIFIED BY '1234';
+CREATE USER IF NOT EXISTS 'product_user'@'%' IDENTIFIED BY '1234';
+CREATE USER IF NOT EXISTS 'point_user'@'%'   IDENTIFIED BY '1234';
+CREATE USER IF NOT EXISTS 'payment_user'@'%' IDENTIFIED BY '1234';
+
+GRANT ALL PRIVILEGES ON `order`.*   TO 'order_user'@'%';
+GRANT ALL PRIVILEGES ON `product`.* TO 'product_user'@'%';
+GRANT ALL PRIVILEGES ON `point`.*   TO 'point_user'@'%';
+GRANT ALL PRIVILEGES ON `payment`.* TO 'payment_user'@'%';
+
+FLUSH PRIVILEGES;
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "msa"
+
+include("common", "order", "product", "point", "payment")
